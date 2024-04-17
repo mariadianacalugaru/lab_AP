612105205159340a5ac50fa711ce77ea6c872265
Pigiby <pgb_2000@outlook.it>
2024-04-17 18:42:10 +0200

    Revert "search country"
    This reverts commit 5f70249f0a897cbce75083e502757948a7ffbae9.

--- nutriverse_lab/nutriverse/src/component/search_city/SearchBarCity.js
+++ nutriverse_lab/nutriverse/src/component/search_city/SearchBarCity.js
@@ -2,19 +2,23 @@ import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
 
-const SearchBarCity = ({setResults,country,city,setCity,selectedCountry,setSelectedCountry}) => {
+const SearchBarCity = ({setResults,country,city,setCity}) => {
     
     const fetchData = async (value) => {
           await fetch("https://countriesnow.space/api/v0.1/countries/cities",{
             method: "POST",
+            mode: 'cors', 
+            cache: 'no-cache',
             headers: {
                 'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "http://localhost:3000",
             },
+            referrerPolicy: 'no-referrer',
             body: JSON.stringify({"country": country})
           })
             .then(response => response.json())
-            .then(json => {
-              const results = json.data.filter((city) => { 
+              .then(json => {
+                  const results = json.data.filter((city) => {
                     return city && city.toLowerCase().startsWith(value.toLowerCase())
                 });
                 setResults(results);
@@ -32,11 +36,11 @@ const SearchBarCity = ({setResults,country,city,setCity,selectedCountry,setSelec
   return (
       <div className='input-wrapper'>
           <FaSearch id="search-icon-country" />
-          {selectedCountry && <input id="input" placeholder='Choose the city of your study'
+          <input id="input" placeholder='Choose the city of your study'
               value={city}
               onChange={(e) => handleChange(e.target.value)}
               
-          />}
+          />
       </div>
   )
 }
