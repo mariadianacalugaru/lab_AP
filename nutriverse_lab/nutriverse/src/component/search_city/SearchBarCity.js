de5497283bc725a52eaa0a4a288930a44906a0f8
Pigiby <pgb_2000@outlook.it>
2024-04-17 18:43:04 +0200

    Revert "searchcity"
    This reverts commit 49a663203680be5029ec736833ea52bd2edacaa6.

--- nutriverse_lab/nutriverse/src/component/search_city/SearchBarCity.js
+++ nutriverse_lab/nutriverse/src/component/search_city/SearchBarCity.js
@@ -2,23 +2,19 @@ import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
 
-const SearchBarCity = ({setResults,country,city,setCity}) => {
+const SearchBarCity = ({setResults,country,city,setCity,selectedCountry,setSelectedCountry}) => {
     
     const fetchData = async (value) => {
           await fetch("https://countriesnow.space/api/v0.1/countries/cities",{
             method: "POST",
-            mode: 'cors', 
-            cache: 'no-cache',
             headers: {
                 'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": "http://localhost:3000",
             },
-            referrerPolicy: 'no-referrer',
             body: JSON.stringify({"country": country})
           })
             .then(response => response.json())
-              .then(json => {
-                  const results = json.data.filter((city) => {
+            .then(json => {
+              const results = json.data.filter((city) => { 
                     return city && city.toLowerCase().startsWith(value.toLowerCase())
                 });
                 setResults(results);
@@ -36,11 +32,11 @@ const SearchBarCity = ({setResults,country,city,setCity}) => {
   return (
       <div className='input-wrapper'>
           <FaSearch id="search-icon-country" />
-          <input id="input" placeholder='Choose the city of your study'
+          {selectedCountry && <input id="input" placeholder='Choose the city of your study'
               value={city}
               onChange={(e) => handleChange(e.target.value)}
               
-          />
+          />}
       </div>
   )
 }
--- nutriverse_lab/nutriverse/src/component/search_city/SearchCity.js
+++ nutriverse_lab/nutriverse/src/component/search_city/SearchCity.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import SearchBarCity from './SearchBarCity';
 import SearchResultsListCity from './SearchResultsListCity';
 
-const SearchCity = ({country, city, setCity, listCity, setListCity}) => {
+const SearchCity = ({country, city, setCity, listCity, setListCity, selectedCountry,setSelectedCountry}) => {
       return (
         <div className='SearchCountryComponent'>
-          <SearchBarCity setResults={setListCity} country={country} city={city} setCity={setCity}/>
+          <SearchBarCity setResults={setListCity} country={country} city={city} setCity={setCity} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry}/>
           <SearchResultsListCity setCity={setCity} results={listCity} setList={setListCity} />
       </div>
     )
