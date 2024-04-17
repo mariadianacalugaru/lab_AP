d39ade0265d6204b6799b873416d4697e6c9544b
Pigiby <pgb_2000@outlook.it>
2024-04-17 18:33:36 +0200

    voglio morire

--- nutriverse_lab/nutriverse/src/component/SearchCountry.js
+++ nutriverse_lab/nutriverse/src/component/SearchCountry.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 import SearchBarCountry from './SearchBarCountry';
 import SearchResultsListCountry from './SearchResultsListCountry';
 
-const SearchCountry = ({country,setCountry, setListCity, setCity}) => {
+const SearchCountry = ({country,setCountry, setListCity, setCity,setSelectedCountry}) => {
   const [list, setList] = useState([]);
       return (
         <div className='SearchCountryComponent'>
-          <SearchBarCountry setResults={setList} country={country} setCountry={setCountry} setCity={setCity}/>
-          <SearchResultsListCountry setCountry={setCountry} results={list} setList={setList} setListCity={setListCity} setCity={setCity}/>        
+          <SearchBarCountry setResults={setList} country={country} setCountry={setCountry} setCity={setCity} setListCity={setListCity} setSelectedCountry={setSelectedCountry} />
+          <SearchResultsListCountry setCountry={setCountry} results={list} setList={setList} setListCity={setListCity} setCity={setCity} setSelectedCountry={setSelectedCountry} />        
       </div>
     )
   }
